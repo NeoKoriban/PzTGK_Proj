97a9a5c078ef4a2a4747a45fdec851afa5846f5b
Shingen <piotr.kunicki@o2.pl>
2016-06-07 22:14:23 +0200

    Wypakowanie plików menu z zipa.

---
+++ PZTGK/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public int option;
+	void Start () {
+		GetComponent<Renderer>().material.color = Color.cyan;
+	}
+
+	void OnMouseEnter(){
+		GetComponent<Renderer>().material.color = Color.yellow;
+	}
+
+	void OnMouseExit(){
+		GetComponent<Renderer>().material.color = Color.cyan;
+	}
+	void OnMouseUp(){
+		switch(option)
+		{
+		case 0:
+			Application.LoadLevel(1);
+			break;
+		case 3:
+			Application.Quit ();
+			break;
+		}
+	} 
+}
